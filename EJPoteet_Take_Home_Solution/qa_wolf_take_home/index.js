09138b50661d9645180e2cb387c3ea00e6d41757
Emma Jean Poteet <271843@carmax.com>
2026-03-31 13:30:45 -0400

    Extrapolate date comparison to separate loop

--- EJPoteet_Take_Home_Solution/qa_wolf_take_home/index.js
+++ EJPoteet_Take_Home_Solution/qa_wolf_take_home/index.js
@@ -10,51 +10,65 @@ async function sortHackerNewsArticles() {
   // go to Hacker News
   await page.goto("https://news.ycombinator.com/newest");
 
-  //set variables to be compared for chronological order
+  //set variables
+  let articleDate = '';
   let higherArticleDate = '';
   let lowerArticleDate = '';
   let currentArticleIndex = 0;
+  let arrayLoopIndex = 0;
+
+  //create an array to push the dates into
+  let dates = [];
   
   //Takes the date pulled from the html element, and transforms it into a date value type
   function formatDate(date) {
     date = date.slice(0, 19);
     date = new Date(date);
-    return date
+    return date;
   }
 
   //function to compare and log dates for chronological order. Note that the older date (i.e. the one from the lower article) should be passed in first.
   function compareDates(firstDate, secondDate, articleNumber) {
-    if (firstDate < secondDate) {
-      console.log(`Success: article #${articleNumber + 1} is in descending chronological order. ${secondDate} > ${firstDate}`)
-      return
+    if (firstDate < secondDate || +firstDate === +secondDate) {
+      console.log(`Success: article #${articleNumber + 1} is in descending chronological order. ${secondDate} > ${firstDate}`);
+      return;
     } else if (firstDate > secondDate) {
-      throw new Error (`Error: article #${articleNumber + 1} is not in the expected chronological order. ${secondDate} < ${firstDate}`)
+      throw new Error (`Error: article #${articleNumber + 1} is not in the expected chronological order. ${secondDate} < ${firstDate}`);
     } else {
-      throw new Error(`Unexpected result at article #${articleNumber + 1}. Test Failure.  Article #${articleNumber} date: ${secondDate}. Article #${articleNumber + 1} date: ${firstDate}.`)
+      throw new Error(`Unexpected result at article #${articleNumber + 1}. Test Failure.  Article #${articleNumber} date: ${secondDate}. Article #${articleNumber + 1} date: ${firstDate}.`);
     }
   }
- 
-  //loop through the first 100 articles, and call the functions that we set up earlier to fromat and compare dates.
+
+
+  //loop through the first 100 articles, and push their posted dates into our array to keep them in order grabbed from the website
   for (i = 0; i < 100; i++) {
-    //if it's the very first article, we don't need to compare to anything yet.
-    if (i == 0) {
-      lowerArticleDate = formatDate(await page.locator('.age').nth(currentArticleIndex).getAttribute('title'));
-      currentArticleIndex++
-    }
-    //every 30 articles, we need to click the More button to load the next 30 articles.
-    else if (i % 30 === 0) {
+    //every 30 articles we need to click the More button to load the next 30 articles. There's a chance one of the article titles could contain the word "more", so we want to grab the last element with that text.
+    if (i % 30 === 0) {
       await page.getByText('More').last().click();
-      currentArticleIndex = 0
-      higherArticleDate = lowerArticleDate;
-      lowerArticleDate = formatDate(await page.locator('.age').nth(currentArticleIndex).getAttribute('title'));
-      compareDates(lowerArticleDate, higherArticleDate, i);
-      currentArticleIndex++
-    //otherwise we get the happy path
+      currentArticleIndex = 0;
+      articleDate = formatDate(await page.locator('.age').nth(currentArticleIndex).getAttribute('title'));
+      dates.push(articleDate);
+      currentArticleIndex++;
+    //otherwise we can just proceed to the next article, and grab the date
     } else {
-      higherArticleDate = lowerArticleDate;
-      lowerArticleDate = formatDate(await page.locator('.age').nth(currentArticleIndex).getAttribute('title'));
-      compareDates(lowerArticleDate, higherArticleDate, i);
-      currentArticleIndex++
+      articleDate = formatDate(await page.locator('.age').nth(currentArticleIndex).getAttribute('title'));
+      dates.push(articleDate);
+      currentArticleIndex++;
+    }
+  }
+
+  //finally we can loop through our array, and compare the dates
+  for (date of dates) {
+    //If this is the first loop, there is nothing to yet compare
+    if (arrayLoopIndex === 0) {
+    lowerArticleDate = date;
+    arrayLoopIndex++;
+    //and now we're pod racing. Sets the previous array index to a different variable, then compares it to the current array index date.
+    } else {
+    higherArticleDate = lowerArticleDate;
+    lowerArticleDate = date;
+    compareDates(lowerArticleDate, higherArticleDate, arrayLoopIndex);
+    arrayLoopIndex++;
     }
   }
 
